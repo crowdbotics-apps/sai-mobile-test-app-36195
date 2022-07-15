855246fe19499ec13f83b6c0b73984cba0400af3
Crowdbotics <team@crowdbotics.com>
2022-07-15 05:57:32 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/sai_mobile_test_app_36195/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sai_mobile_test_app_36195;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "sai_mobile_test_app_36195";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "sai_mobile_test_app_36195",
+  url: "https://sai_mobile_test_app_36195.botics.co",
+  api: "https://sai_mobile_test_app_36195.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
